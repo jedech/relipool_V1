b8607806b9b2c460de45bd11b06631455167a637
jedech <167635390+jedech@users.noreply.github.com>
2025-06-19 19:57:32 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+import { relayInit } from "https://esm.sh/nostr-tools";
+
+const relay = relayInit("wss://relay.damus.io");
+await relay.connect();
+
+const sub = relay.sub([
+  {
+    kinds: [30142],
+    limit: 10, // Anzahl begrenzen
+  },
+]);
+
+const eventsDiv = document.getElementById("events");
+eventsDiv.innerHTML = "";
+
+sub.on("event", (event) => {
+  const div = document.createElement("div");
+  div.className = "event";
+  div.innerHTML = `
+    <strong>PubKey:</strong> ${event.pubkey.slice(0, 16)}…<br />
+    <strong>Inhalt:</strong> <pre>${event.content}</pre>
+  `;
+  eventsDiv.appendChild(div);
+});
